727d0fcad97d0c7c891d1e9628140c0fe65ea31f
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-01-30 13:17:46 +0100

    la til GitlabCISupport fra eksempel-repoet

--- tdt4140-gr1806/app.ui/src/test/java/tdt4140/gr1806/app/ui/FxAppTest.java
+++ tdt4140-gr1806/app.ui/src/test/java/tdt4140/gr1806/app/ui/FxAppTest.java
@@ -1,6 +1,7 @@
 package tdt4140.gr1806.app.ui;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -8,8 +9,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tdt4140.gr1806.app.ui.GitlabCISupport;
 
 public class FxAppTest extends ApplicationTest {
+	
+	@BeforeClass
+    public static void headless() {
+    		if (Boolean.valueOf(System.getProperty("gitlab-ci", "false"))) {
+    			GitlabCISupport.headless();
+    		}
+    }
 
 	@Override
     public void start(Stage stage) throws Exception {
