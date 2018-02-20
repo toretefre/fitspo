4f05a56b9b8201430c7129189529ca23b7b80d81
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-02-20 15:50:40 +0100

    Lagt til ConnectionManager for database

---
+++ tdt4140-gr1806/app.core/src/main/java/tdt4140/gr1806/app/core/ConnectionManager.java
@@ -0,0 +1,30 @@
+package tdt4140.gr1806.app.core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ConnectionManager {
+
+    public static Connection conn;
+
+    /**
+    Connection connect
+    *
+    * Connects to the database.
+    */
+    public static Connection connect(){
+        try {
+            String url = "jdbc:mysql://mysql.stud.ntnu.no:3306/matiasre_FitsPo";
+            conn = DriverManager.getConnection(url, "matiasre_gruppe6", "123safari");
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+}
