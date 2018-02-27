4bffd760f25e005249c27e1f4d95fa9ee61aa191
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-02-27 11:55:57 +0100

    Update FxAppTest.java

--- tdt4140-gr1806/app.ui/src/test/java/tdt4140/gr1806/app/ui/FxAppTest.java
+++ tdt4140-gr1806/app.ui/src/test/java/tdt4140/gr1806/app/ui/FxAppTest.java
@@ -22,7 +22,7 @@ public class FxAppTest extends ApplicationTest {
 
 	@Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FitspoApp.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
