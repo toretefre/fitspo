d9da9d0ef68c7527938e730dcd09f16436ebf75a
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-03-01 12:00:07 +0100

    må commite for å få merget med issue 17

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController.java
@@ -1,4 +1,37 @@
 package tdt4140.gr1806.app.ui;
 
-public class FitspoAppController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class FitspoAppController implements Initializable {
+	@FXML ScrollPane container;
+	@FXML VBox content;
+	
+	
+	public void homeLanding() {
+		HBox person = new HBox();
+		for (int i=0; i<2; i++) {
+			
+			Label name = new Label("mann"+i);
+			Label skritt = new Label("100"+i);
+			person.getChildren().addAll(name,skritt);
+			
+		}
+		content.getChildren().add(person);
+		container.setContent(container);
+	}
+	
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		homeLanding();
+	}
 }
