0442e9f25a7de471bcb15f5b74d18e4e27e3c5fb
Åsmund <aasmusta@stud.ntnu.no>
2018-03-02 12:17:59 +0100

    cleaning up pom files and checking if jetty starts

--- tdt4140-gr1806/web.server/src/main/java/tdt4140/gr1806/web/server/ResourceConfiguration.java
+++ tdt4140-gr1806/web.server/src/main/java/tdt4140/gr1806/web/server/ResourceConfiguration.java
@@ -1,5 +1,6 @@
 package tdt4140.gr1806.web.server;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ResourceConfiguration extends ResourceConfig{
@@ -7,6 +8,9 @@ public class ResourceConfiguration extends ResourceConfig{
 	public ResourceConfiguration() {
 		packages("tdt4140.gr1806.web.server");
 		register(StepReciever.class);
+		
+		// Enables Jackson-based JSON support
+		register(JacksonFeature.class);
 	}
 
 }
