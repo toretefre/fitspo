bfec684ae8d813fa3c68be21f783ecd4780316a0
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-03-13 11:05:40 +0100

    actually fixing failed build

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController_trainer.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController_trainer.java
@@ -31,7 +31,7 @@ public class FitspoAppController_trainer implements Initializable {
 	 * @see tdt4140.gr1806.app.core.Trainer.java
 	 */
 	private void personalInfo(ArrayList<ArrayList<String>> customers) {
-		for (int i=0; i<customers.; i++) {
+		for (int i=0; i<customers; i++) {
 			HBox person = new HBox();
 			person.setId("personbox" + i % 2);
 			person.setPrefWidth(container.getPrefWidth());
