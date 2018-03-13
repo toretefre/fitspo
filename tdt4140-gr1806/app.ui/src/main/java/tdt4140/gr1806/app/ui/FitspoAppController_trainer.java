a9b2a5f33400a00a72b5476e93a5351df8c4d1bb
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-03-13 11:11:37 +0100

    unneccesary anyways

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController_trainer.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController_trainer.java
@@ -13,7 +13,7 @@ import javafx.scene.layout.VBox;
 import tdt4140.gr1806.app.core.Trainer;
 /**
  * FitspoAppController Class use the Trainer Class to represent an updated list of customers and their total steps.
- * Controlling the FitspoApp.fxml
+ * Controlling the FitspoApp_trainer.fxml
  * @author Magnus
  * Modified by Tore
  * @version 1.0
@@ -23,20 +23,6 @@ import tdt4140.gr1806.app.core.Trainer;
 public class FitspoAppController_trainer implements Initializable {
 	@FXML private ScrollPane container;
 	@FXML private VBox content;
-	
-	/**
-	 * uses the Trainer Class method getCustomer which returns a ArrayList<ArrayList<String>> of customers and puts it into
-	 * the ScrollPane from FitspoApp.fxml. Sets differents ID for styling purposes.
-	 * @param customers
-	 * @see tdt4140.gr1806.app.core.Trainer.java
-	 */
-	private void personalInfo(ArrayList<ArrayList<String>> customers) {
-		for (int i=0; i<customers.size(); i++) {
-			HBox person = new HBox();
-			person.setId("personbox" + i % 2);
-			person.setPrefWidth(container.getPrefWidth());
-		}	
-	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
