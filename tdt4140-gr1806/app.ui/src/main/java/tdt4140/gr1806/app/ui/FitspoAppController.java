188efaf437e56c41cd9da5e49da0ddea287b7b01
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-03-19 11:26:24 +0100

    cleaned code

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController.java
@@ -1,26 +1,13 @@
 package tdt4140.gr1806.app.ui;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import tdt4140.gr1806.app.core.Customer;
-import tdt4140.gr1806.app.core.Trainer;
 import tdt4140.gr1806.app.ui.MenuController;
+
+
 /**
  * FitspoAppController Class use the Trainer Class to represent an updated list of customers and their total steps.
  * Controlling the FitspoApp.fxml
@@ -29,6 +16,8 @@ import tdt4140.gr1806.app.ui.MenuController;
  * @see tdt4140.gr1806.app.core.Trainer.java
  * @see tdt4140.gr1806.app.ui.FitspoApp.fxml
  */
+
+
 public class FitspoAppController implements Initializable {
 	// All different controllers
 	@FXML private CustomerViewController customerViewController;
@@ -41,13 +30,10 @@ public class FitspoAppController implements Initializable {
 	 * @see tdt4140.gr1806.app.core.Trainer.java
 	 */
 
+	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("Start");
 		menuBarController.init(this);
 		customerViewController.init(this);
 	}
-	
-
-	
-	
 }
