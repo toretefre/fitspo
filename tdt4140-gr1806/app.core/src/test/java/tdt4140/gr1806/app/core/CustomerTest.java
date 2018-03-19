413a0309425316e762b83a6aec3bcaee8cf532af
adrianhofseth <adrianhh@stud.ntnu.no>
2018-03-19 10:35:24 +0100

    #28 Made some tests

--- tdt4140-gr1806/app.core/src/test/java/tdt4140/gr1806/app/core/CustomerTest.java
+++ tdt4140-gr1806/app.core/src/test/java/tdt4140/gr1806/app/core/CustomerTest.java
@@ -1,11 +1,15 @@
 package tdt4140.gr1806.app.core;
 
 
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+
 
 /**
  * This is a class for ONLY testing Customer's methods,
@@ -19,22 +23,11 @@ public class CustomerTest {
 	@Test
 	public void testGetCustomer() {
 		ArrayList<Customer> testCustomer = Trainer.getCustomers();
-		for(Customer customer : testCustomer) {
-			assertEqual("Berdfsit".equals(customer.getName()));
-			
+		for(Customer c : testCustomer) {
+			if(c.getName().equals("Berit")) {
+				assertTrue("This will be true", true);
+			}
 		}
-		assertNotNull("List should not be null", testCustomer);
-		//assertThat(testCustomer, containsInAnyOrder(
-		//		hasProperty("name", is("Berit"))
-		//		));
-		
-	}
-
-
-
-	private void assertEqual(boolean equals) {
-		// TODO Auto-generated method stub
-		
 	}
 
 
@@ -42,7 +35,14 @@ public class CustomerTest {
 	@Test
 	public void testGetCustomerNotNull() {
 		ArrayList<Customer> testCustomer = Trainer.getCustomers();
-		assertNotNull("List should not be null", testCustomer);
+		//assertNotNull("List should not be null", testCustomer);
+		for(Customer c : testCustomer) {
+			List<Customer> objekter = new ArrayList<>();
+			if(c.getName().equals("Berit")) {
+				objekter.add(new Customer("Berit", 222));
+		assertNotNull("List should not be null", objekter);
+			}
+		}
 		
 		
 	}
