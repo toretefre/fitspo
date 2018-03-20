98a552c9fcdb3cfbe93e65d14d12f171bf402899
henriean <henriean@users.noreply.github.com>
2018-03-20 09:23:54 +0100

    #32 Started on CustomerRepository.

--- tdt4140-gr1806/app.core/src/main/java/tdt4140/gr1806/app/core/Customer.java
+++ tdt4140-gr1806/app.core/src/main/java/tdt4140/gr1806/app/core/Customer.java
@@ -2,256 +2,57 @@ package tdt4140.gr1806.app.core;
 /**
  * Instances of this class will represent a customer
  */
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-//import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Timestamp;
-import java.util.Calendar;
 
-import com.sun.javafx.image.impl.ByteIndexed.Getter;
-//import java.sql.Statement;
 
-public class Customer extends ActiveDomainObject{
+
+public class Customer{
 	
-	private int steps, height;
+	private int id;
+	private int height;
 	private double weight;
-	private int id = -1;
-	private String name, birthDate, dateRegistered, telephone;
-	private Gender gender;
+	private String name;
+	private String birthDate;
+	private String dateRegistered;
+	private String telephone;
+	private String gender;
+	
+	//TODO: Checks for set-methods?
 
 	/**
 	 * 
-	 * @param id ID corresponding to customer row in DB
-	 * @param name Customer name
-	 * 
-	 * TODO: Maybe this construtor only should take in id and fetch data from the database?
+	 * @param name
+	 * @param birthDate
+	 * @param telephone
+	 * @param gender
+	 * @param height
+	 * @param weight
 	 */
 	
-	public static void main(String [ ] args){
-		Connection conn = ConnectionManager.connect();
-		/*Customer c = new Customer(19);
-		c.init(conn);
-		System.out.println(c.getName());
-		System.out.println(c.getGender());
-		c.setName("Henriette Andersen");
-		c.save(conn); */
-		
-		/*Customer n = new Customer();
-		n.setName("Testie Mc Test");
-		n.setTelephone("12345678");
-		n.save(conn);
-		System.out.println(n.getId());*/
+	public Customer(String name, String gender, String telephone, String birthDate, int height, double weight) {
+		this.name = name;
+		this.birthDate = birthDate;
+		this.telephone = telephone;
+		this.gender = gender;
+		this.height = height;
+		this.weight = weight;
 	}
 	
-
-	
-	/**
-	 * Adds a new customer to the database, where only the name is set.
-	 * 
-	 * @param name; name of a new customer.
-	 * @return auto generated id if there is made a new row in the DB, -1 if not.
-	 * 
-	 */
-	/*
-	public static int addCustomer(String name) {
-		if (name == null) {
-			System.err.println("Customer name cannot be null. No new row was added.");
-			return -1;
-		}
-		try {
-			String sql = "insert into Customer (name) values ('" + name + "')";
-			Connection connection = ConnectionManager.connect();
-			PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			pstmt.executeUpdate();
-			//The next code gets the auto-generated key:
-			ResultSet rs = pstmt.getGeneratedKeys();
-			rs.next();
-			int auto_id = rs.getInt(1);
-			return auto_id;
-		} catch (SQLException e) {
-			System.err.println("Could not save to database.");
-			e.printStackTrace();
-			return -1;
-		}	
-	}*/
-	
-	
-	/*
-	public static void addCustomer(String name, Gender gender, int telephone, Date birthDate, int height, int weight) {
-		try {
-			String sql = "insert into Customer values ("
-					+ "null,"
-					+ "'" + name + "',"
-					+ "'" + gender + "',"
-					+ "'" + "NOW()" + "',"
-					+ "'" + telephone + "',"
-					+ "'" + birthDate + "',"
-					+ "'" + height + "',"
-					+ "'" + weight + "'"
-					+ ")";
-			Connection connection = ConnectionManager.connect();
-			PreparedStatement pstmt = connection.prepareStatement(sql); //, Statement.RETURN_GENERATED_KEYS);
-			pstmt.executeUpdate();
-			// The next code gets the auto-generated key:
-			//ResultSet rs = pstmt.getGeneratedKeys();
-			//rs.next();
-			//int auto_id = rs.getInt(1);
-		} catch (SQLException e) {
-			System.err.println("Could not save to database.");
-			e.printStackTrace();
-		}	
-	}
-	*/
-	
-	/*
-	public static void addStepsToday(int id, int steps) {
-		try {
-			Date sqlDate = new Date(System.currentTimeMillis());
-			String sql = "insert into StepsOnDay (customerId, steps, walkDay) values ("
-					+ "'" + id + "',"
-					+ "'" + steps + "',"
-					+ "'" + sqlDate + "'"
-					+ ")";
-			Connection connection = ConnectionManager.connect();
-			PreparedStatement pstmt = connection.prepareStatement(sql);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.err.println("Could not save to database.");
-			e.printStackTrace();
-		}	
-	}
-	
-	
-	public static void addSteps(int id, int steps, String date) {
-		// TODO: Sjekk at string er på riktig form, om vi ikke lar sql ta dette.
-		try {
-			String sql = "insert into StepsOnDay (customerId, steps, walkDay) values ("
-					+ "'" + id + "',"
-					+ "'" + steps + "',"
-					+ "'" + date + "'"
-					+ ")";
-			Connection connection = ConnectionManager.connect();
-			PreparedStatement pstmt = connection.prepareStatement(sql); 
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.err.println("Could not save to database.");
-			e.printStackTrace();
-		}	
-	}*/
-
-
-	@Override
-	public void init(Connection conn) {
-		try {
-			String query = "select name, gender, dateRegistered, telephone, birthDate, height, weight from Customer where id=?";
-			PreparedStatement pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, this.id);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				this.name =  rs.getString("name");
-				try {
-					this.gender = Gender.cast(rs.getString("gender"));
-				} catch(Exception e){
-				}
-				this.dateRegistered = rs.getString("dateRegistered");
-				try {
-					this.telephone = rs.getString("telephone");
-				} catch(Exception e){
-				}
-				try {
-					this.birthDate = rs.getString("birthDate");
-				} catch(Exception e){
-				}
-				try {
-					this.height = rs.getInt("height");
-				} catch(Exception e){
-				}
-				try {
-					this.weight = rs.getDouble("weight");
-				} catch(Exception e){
-				}
-			}
-			} catch (Exception e) {
-            	System.out.println("db error during select of customer with id = "+this.id);
-            	System.err.print(e);
-            	return;
-            	}
-		}
-	
-
-	@Override
-	public void refresh(Connection conn) {
-		this.init(conn);
-	}
-
-
-	@Override
-	public void save(Connection conn) {
-		if (this.id == -1) {
-			try {
-				String update = "insert into Customer (name, gender, telephone, birthDate, height, weight) values (?, ?, ?, ?, ?, ?);";
-				PreparedStatement pstmt = conn.prepareStatement(update,Statement.RETURN_GENERATED_KEYS);
-				pstmt.setString(1, this.name);
-				pstmt.setString(2, this.gender.toString());
-				pstmt.setString(3, this.telephone);
-				pstmt.setString(4, this.birthDate);
-				if (this.height == 0) {
-					pstmt.setString(5, null);
-				} else {
-					pstmt.setInt(5, this.height);
-				}
-				if (this.height == 0.0) {
-					pstmt.setString(6, null);
-				} else {
-					pstmt.setDouble(6, this.weight);
-				}
-				pstmt.executeUpdate();
-				ResultSet rs = pstmt.getGeneratedKeys();
-				rs.next();
-				this.id = rs.getInt(1);
-			} catch (Exception e) {
-	        	System.out.println("db error during inserting of new customer");
-	        	System.err.print(e);
-	        	return;
-	        	}
-		} else {
-			try {
-				String update = "update Customer set name=?, gender=?, telephone=?, birthDate=?, height=?, weight=? where id=?";
-				PreparedStatement pstmt = conn.prepareStatement(update);
-				pstmt.setString(1, this.name);
-				pstmt.setString(2, this.gender.toString());
-				pstmt.setString(3, this.telephone);
-				pstmt.setString(4, this.birthDate);
-				if (this.height == 0) {
-					pstmt.setString(5, null);
-				} else {
-					pstmt.setInt(5, this.height);
-				}
-				if (this.height == 0.0) {
-					pstmt.setString(6, null);
-				} else {
-					pstmt.setDouble(6, this.weight);
-				}
-				pstmt.setInt(7, this.id);
-				pstmt.executeUpdate();
-			} catch (Exception e) {
-	        	System.out.println("db error during insert of customer with id = "+this.id);
-	        	System.err.print(e);
-	        	return;
-	        	}
-		}
-	}
-	
-	
-	public Customer(int id) {
+	public Customer(int id, String name, String gender, String dateRegistered, String telephone, String birthDate, int height, double weight) {
 		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.telephone = telephone;
+		this.gender = gender;
+		this.dateRegistered = dateRegistered;
+		this.height = height;
+		this.weight = weight;
 	}
-	public Customer() {
+	
+	public static void main(String[] args) {
+		Customer c = new Customer("Navn", null, "Telefon", null, 0, 0);
+		System.out.println(c);
 	}
+	
 	
 	public void setId(int id) {
 		this.id = id;
@@ -265,14 +66,7 @@ public class Customer extends ActiveDomainObject{
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public int getSteps() {
-		return steps;
-	}
-	public void setSteps(int steps) {
-		this.steps = steps;
-	}
+	}	
 	
 	public int getHeight() {
 		return height;
@@ -296,22 +90,27 @@ public class Customer extends ActiveDomainObject{
 	}
 
 
-	public String getBirthdate() {
+	public String getBirthDate() {
 		return birthDate;
 	}
 	public void setBirthdate(String birthdate) {
 		this.birthDate = birthdate;
 	}
 	
+	
 	public String getDateRegistered() {
 		return dateRegistered;
 	}
 	
+	public void setDateRegistered(String date) {
+		this.dateRegistered = date;
+	}
+	
 
-	public Gender getGender() {
+	public String getGender() {
 		return gender;
 	}
-	public void setGender(Gender gender) {
+	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
---
+++ tdt4140-gr1806/app.core/src/main/java/tdt4140/gr1806/app/core/CustomerRepository.java
@@ -0,0 +1,130 @@
+package tdt4140.gr1806.app.core;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Implements methods for use to add customer.
+ * @author henriette_andersen
+ *
+ */
+
+public class CustomerRepository extends ConnectionManager {
+	public static void main(String[] args) {
+	}
+	
+	public CustomerRepository() {
+		connect();
+	}
+	
+	
+	private void setIfNotZero(PreparedStatement p, int index, int integer) throws SQLException {
+		if (integer != 0) {
+			p.setInt(index, integer);
+		}
+	}
+	
+	
+	public Customer saveCustomer(Customer customer) {
+		try {
+			String update = "insert into Customer "
+					+ "(name, "
+					+ "gender, "
+					+ "telephone, "
+					+ "birthDate, "
+					+ "height, "
+					+ "weight) "
+					+ "values (?, ?, ?, ?, ?, ?);";
+
+			PreparedStatement pstmt = conn.prepareStatement(update,Statement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, customer.getName());
+			pstmt.setString(2, customer.getGender());
+			pstmt.setString(3, customer.getTelephone());
+			pstmt.setString(4, customer.getBirthDate());
+			this.setIfNotZero(pstmt, 5, customer.getHeight());
+			this.setIfNotZero(pstmt, 6, (int) customer.getWeight());
+			pstmt.executeUpdate();
+			ResultSet rs = pstmt.getGeneratedKeys();
+			rs.next();
+			customer.setId(rs.getInt(1));
+			customer.setDateRegistered(rs.getString(4));
+		} catch (Exception e) {
+        	System.out.println("db error during inserting of new customer");
+        	System.err.print(e);
+        	}
+		return customer;
+	}
+	
+	
+	public void addStepsToCustomer(Customer customer, int steps, String date) {
+		//TODO: String format check, not minus int check
+		try {
+			String insert = "insert into StepsOnDay (customerId, steps, walkDay) values (?, ?, ?);";
+			PreparedStatement pstmt = conn.prepareStatement(insert);
+			pstmt.setInt(1, customer.getId());
+			pstmt.setInt(2, steps);
+			pstmt.setString(3, date);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.err.println("Could not save to database.");
+			e.printStackTrace();
+		}	
+	}
+	
+	
+	
+	private Customer createCustomerFromResultSet(ResultSet rs) throws SQLException {
+		int id = rs.getInt("id");
+		String name = rs.getString("name");
+		String gender = rs.getString("gender");
+		String date = rs.getString("dateRegistered");
+		String telephone = rs.getString("telephone");
+		String bDate = rs.getString("birthDate");
+		int height = rs.getInt("height");
+		double weight = rs.getInt("weight");
+		
+		Customer customer = new Customer(id, name, gender, date, telephone, bDate, height, weight);
+		return customer;
+	}
+		
+		
+	
+	public ArrayList<Customer> findAllCustomers() {
+		ArrayList<Customer> customers = new ArrayList<Customer>();
+		try {
+			String query = "select * from Customer";
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Customer customer = this.createCustomerFromResultSet(rs);
+				customers.add(customer);
+			}
+			} catch (Exception e) {
+            	System.out.println("db error during selection of customers");
+            	System.err.print(e);
+            	}
+		return customers;
+	}
+	
+	
+	
+	public void delete(Customer customer) {
+		try {
+			String delete = "delete from Customer where id=?";
+			PreparedStatement pstmt = conn.prepareStatement(delete);
+			pstmt.setInt(1, customer.getId());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("db error during selection of customers");
+        		System.err.print(e);
+		}
+	}
+	
+	
+	public void getTotalStepsFromCustomer(Customer customer) {
+		String query = ""
+	}
+	
+	
+	
+}
