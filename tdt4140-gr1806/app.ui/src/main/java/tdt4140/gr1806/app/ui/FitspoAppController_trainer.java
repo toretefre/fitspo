0065a5dcb75349dec6622fd7b67f59492f3c759a
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-04-09 12:38:36 +0200

    #58 #59 Added method in trainer controller and updated the trainer fxml file to resolve issue 58,59

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController_trainer.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/FitspoAppController_trainer.java
@@ -1,11 +1,17 @@
 package tdt4140.gr1806.app.ui;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import com.mysql.fabric.xmlrpc.base.Data;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
@@ -27,14 +33,16 @@ public class FitspoAppController_trainer {
 	@FXML private ScrollPane container;
 	@FXML private VBox content;
 	@FXML private Label userName;
+	@FXML private DatePicker from;
+	@FXML private DatePicker to;
+	ArrayList<String[]> data = new ArrayList<>();
 	private CustomerRepository customerRepository = new CustomerRepository();
-
+	private Customer cus;
+	
 	private void loadCustomerData(Customer selectedPerson) {
+		cus = selectedPerson;
 		userName.setText(selectedPerson.getName());
 		
-		// The reason a lot is commented out is because the Customer can be constructed without the fields
-		// resulting in errors here (.toString() for example). This should be solved in Customer and not here.
-		ArrayList<String[]> data = new ArrayList<>();
 		data.add(new String[]{"Telephone", selectedPerson.getTelephone()});
 		data.add(new String[]{"Birthdate", selectedPerson.getBirthDate()});
 		data.add(new String[]{"Gender", selectedPerson.getGender()});
@@ -46,6 +54,9 @@ public class FitspoAppController_trainer {
 		for (int i = 0; i < data.size(); i++) {
 			HBox dataRow = new HBox();
 			dataRow.setId("datarow" + i % 2);
+			if(i == 5) { //Easy access to change steps in updateCustomerSteps
+				dataRow.setId("steps");
+			}
 			dataRow.setPrefWidth(container.getPrefWidth());
 			Label name = new Label(data.get(i)[0]);
 			name.setId("personboxLabel");
@@ -55,10 +66,41 @@ public class FitspoAppController_trainer {
 			
 			dataRow.getChildren().addAll(name,skritt);
 			content.getChildren().add(dataRow);
-			
+		}
+	}
+	
+	/**
+	 * Uses CustomerRepository.getTotalStepsInDateRange to find steps in date set by the user if both from and to are set.
+	 * Changes the HBox which specifies the steps registred on user.
+	 * @param event
+	 * @throws IOException
+	 */
+	
+	@FXML public void updateCustomerSteps(ActionEvent event) throws IOException {
+		System.out.println("Working");
+		String[] update = new String[] {"Steps", "44"};
+		LocalDate fromDate = from.getValue();
+		LocalDate toDate = to.getValue();
+		
+		if(fromDate != null && toDate != null) {
+			int steps = CustomerRepository.getTotalStepsInDateRange(cus, fromDate, toDate);
+			//data.set(5, new String[] {"Steps", Integer.toString(steps)} );
+			HBox dataRow = new HBox();
+			dataRow.setId("1");
+			dataRow.setPrefWidth(container.getPrefWidth());
+			Label name = new Label("Steps");
+			name.setId("personboxLabel");
+			Label step = new Label(Integer.toString(steps));
+			step.setId("personboxSkrittLabel");
+			dataRow.getChildren().addAll(name, step);
+			content.getChildren().set(5, dataRow);
 		}
 		
-
+		
+	}
+	
+	@FXML public void something(ActionEvent event) throws Exception {
+		System.out.println("Something cool happend here");
 	}
 	
 	public void init(Customer target) {
