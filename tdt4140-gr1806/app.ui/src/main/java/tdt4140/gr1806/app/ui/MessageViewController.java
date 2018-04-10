6a599032f51e3d3f3cfdd9c7a66ee26de55ab85a
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-04-10 12:02:45 +0200

    added methods for send and clear message and updateMessageField

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/MessageViewController.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/MessageViewController.java
@@ -1,10 +1,10 @@
 package tdt4140.gr1806.app.ui;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,23 +12,27 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
 import tdt4140.gr1806.app.core.Customer;
 import tdt4140.gr1806.app.core.CustomerRepository;
+import tdt4140.gr1806.app.core.Message;
 
 public class MessageViewController {
 	FitspoAppController fitspo;
 	CustomerRepository cr = new CustomerRepository();
+	Customer customer;
 	@FXML private ComboBox<Customer> customerComboBox;
 	@FXML private Button sendMessageBtn;
 	@FXML private Button clearMessageBtn;
 	@FXML private TextField messageTextField;
 	@FXML private ScrollPane messageBox;
+	@FXML private VBox content;
+	
 	
 	
 	public void changeCenterContent(ActionEvent event) throws IOException {
@@ -39,35 +43,40 @@ public class MessageViewController {
 	}
 	
 	
-	public void updateDropDown(ArrayList<Customer> customers) {
-		customerComboBox = new ComboBox<Customer>();
+	@FXML public void updateDropDown(ArrayList<Customer> customers) {
 		
-		customerComboBox.setConverter(new StringConverter<Customer>() {
-			@Override
-			public String toString(Customer cus) {
-				return cus.getName();
-			}
-			
-			@Override
-			public Customer fromString(String id) {
-				//objektet må sendes videre her tror jeg.
-				return null;
-			}
-			
-		});
-		
-		ObservableList<Customer> cust = cr.findAllCustomerObsList();
-		customerComboBox.setItems(cust);
-		
-	
 		
 	}
 	
 	
+	
+	@FXML public void updateMessageField(Customer customer) {
+		this.customer = customer;
+		ArrayList<Message> messages = cr.getMessages(customer);
+		
+		for(Message m : messages) {
+			TextArea text = new TextArea();
+			text.setText(m.getMessage());
+			content.getChildren().add(text);
+		}
+	}
+	
+	@FXML public void sendMessage(ActionEvent event) throws SQLException {
+		String message = messageTextField.getText();
+		Date sqlDate = new Date(new java.util.Date().getTime());
+		Message m = new Message(sqlDate, customer.getId(), message);
+		cr.saveMessage(m);
+		updateMessageField(customer);
+	}
+	
+	@FXML public void clearMessage(ActionEvent event) {
+		messageTextField.clear();
+	}
+	
 	public void init(FitspoAppController fitspoAppController) {
 		fitspo = fitspoAppController;
 		System.out.println("hallo");
-		updateDropDown(this.cr.findAllCustomers());
+		//updateDropDown(this.cr.findAllCustomers());
 	};
 	
 
