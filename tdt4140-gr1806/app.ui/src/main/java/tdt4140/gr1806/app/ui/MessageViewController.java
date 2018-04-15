e2fd09dc963aa5a94eee42b02d048c6d7587a6a7
Magnus Gaustad <magnuge@stud.ntnu.no>
2018-04-15 14:43:03 +0200

    #41 added new styling. Need to change color on textarea in the messageview

--- tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/MessageViewController.java
+++ tdt4140-gr1806/app.ui/src/main/java/tdt4140/gr1806/app/ui/MessageViewController.java
@@ -51,9 +51,11 @@ public class MessageViewController {
 		content.getChildren().clear();
 		
 		ArrayList<Message> messages = cr.getMessages(this.selectedCustomer);
+		int i=0;
 		for(Message m : messages) {
 			TextArea text = new TextArea();
-			text.setId("messageText");
+			text.setId("messageText"+ i % 2);
+			i++;
 			text.wrapTextProperty();
 			text.setText(m.getDate().toString() + ": \n" +
 					m.getMessage());
@@ -100,7 +102,5 @@ public class MessageViewController {
 				return null;
 			}
 		});
-
 	}
-
 }
